8d5466a8695af4a7efcce4cb64f8d06c69842bdf
Mahdi alikhani <mahdi.alikhani@gmail.com>
2021-10-16 20:07:43 +0330

    Categories part

--- Modules/Contents/Entities/Content.php
+++ Modules/Contents/Entities/Content.php
@@ -4,7 +4,7 @@ namespace Modules\Contents\Entities;
 
 use App\Http\Traits\Dashboard\DeletedBy;
 use App\Models\User;
-use Cviebrock\EloquentSluggable\Sluggable;;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
